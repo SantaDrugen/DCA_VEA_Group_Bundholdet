8689ff1be8136470fbeaeddaa2187f6726d6f87e
Rasmus B.K. Nielsen <313614@via.dk>
2025-05-29 23:06:31 +0200

    DbContext preliminary setup

---
+++ src/EventAssociation.Infrastructure.SqlliteDmPersistence/Context/DesignTimeContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace EventAssociation.Infrastructure.SqlliteDmPersistence.Context;
+
+public class DesignTimeContextFactory : IDesignTimeDbContextFactory<VeaDbContext>
+{
+    public VeaDbContext CreateDbContext(String[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<VeaDbContext>();
+        optionsBuilder.UseSqlite(@"Data Source = VEADatabaseProduction.db");
+        return new VeaDbContext(optionsBuilder.Options);
+    }
+}
---
+++ src/EventAssociation.Infrastructure.SqlliteDmPersistence/Context/VeaDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EventAssociation.Core.Domain.Aggregates.Event;
+using EventAssociation.Core.Domain.Aggregates.Guest;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventAssociation.Infrastructure.SqlliteDmPersistence.Context;
+
+public class VeaDbContext(DbContextOptions options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(VeaDbContext).Assembly);
+    }
+
+    public DbSet<VeaEvent> Events => Set<VeaEvent>();
+
+    public DbSet<VeaGuest> Guests => Set<VeaGuest>();
+}
