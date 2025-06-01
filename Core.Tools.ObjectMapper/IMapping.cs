18187280043bdf162dc4aac5c1c36267f9f00543
Rasmus B.K. Nielsen <313614@via.dk>
2025-06-01 15:47:05 +0200

    WebAPI Preliminary Setup.  - ObjectMapper + Tests  - IMapper  - Basic reference setup

---
+++ Core.Tools.ObjectMapper/IMapping.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Tools.ObjectMapper;
+
+public interface IMapping<TSource,TDst>
+{
+    /// Configure mapping from source to destination.
+    /// The implementation will be invoked by the ObjectMapper to apply special rules.
+    /// <param name="source">The source object.</param>
+    /// <param name="destination">The destination object to populate.</param>
+    void Configure(TSource source, TDst destination);
+}
